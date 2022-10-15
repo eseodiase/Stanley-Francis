41184a7bb13a432b94c29d8d05b17634d66cebe9
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-16 00:58:38 +0100

    home.js

--- pages/home.js
+++ pages/home.js
@@ -10,49 +10,49 @@ const home =()=>{
                 <p id="quote" style="font-size:18px; margin-bottom:0">
                   Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
                 </p>
-                <img src="assets/img/testimonials/testimonials-1.jpg" class="home-img" alt="">
+                <img src="images/pic1.jpg" class="home-img" alt="">
 
               </div>
-            </div><!-- End testimonial item -->
+            </div>
 
             <div class="swiper-slide">
               <div class="home-item" data-aos="fade-up" data-aos-delay="100">
                <p id="quote" style="font-size:18px; margin-bottom:0">
                     Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
                 </p>
-                <img src="assets/img/testimonials/testimonials-2.jpg" class="home-img" alt="">
+                <img src="images/pic2.jpg" class="home-img" alt="">
 
               </div>
-            </div><!-- End testimonial item -->
+            </div>
 
             <div class="swiper-slide">
               <div class="home-item" data-aos="fade-up" data-aos-delay="200">
                 <p id="quote" style="font-size:18px; margin-bottom:0">
                   Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
                 </p>
-                <img src="assets/img/testimonials/testimonials-3.jpg" class="home-img" alt="">
+                <img src="images/pic3.jpg" class="home-img" alt="">
 
               </div>
-            </div><!-- End testimonial item -->
+            </div>
 
             <div class="swiper-slide">
               <div class="home-item" data-aos="fade-up" data-aos-delay="300">
                 <p id="quote" style="font-size:18px; margin-bottom:0">
                   Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum
                 </p>
-                <img src="assets/img/testimonials/testimonials-4.jpg" class="home-img" alt="">
+                <img src="images/pic1.jpg" class="home-img" alt="">
               </div>
-            </div><!-- End testimonial item -->
+            </div>
 
             <div class="swiper-slide">
               <div class="home-item" data-aos="fade-up" data-aos-delay="400">
                 <p id="quote" style="font-size:18px; margin-bottom:0">
                   Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi 
                 </p>
-                <img src="assets/img/testimonials/testimonials-5.jpg" class="home-img" alt="">
+                <img src="images/pic2.jpg" class="home-img" alt="">
 
               </div>
-            </div><!-- End testimonial item -->
+            </div>
 
 
       </div>
